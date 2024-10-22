49767ec7ad21395385748a215984f24ce1a328a3
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2024-10-22 07:26:03 +0200

    logic for creating new ramen added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,39 @@ const handleClick = (ramen) => {
 
 const addSubmitListener = () => {
   // Add code
+  function saveNewRamen(){(id, newName, newRestaurant, newImage, newRating, newComment) => {
+
+    const newRamen = {
+      name: newName,
+      restaurant: newRestaurant,
+      image : newImage,
+      rating: parseFloat(newRating), // Ensure amount is numeric
+      comment: newComment
+    };
+
+    fetch(`${'http://localhost: 3000URL'}/${id}`, {
+      method: "PATCH", // Using PATCH for partial updates
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newRamen)
+    })
+      .then(response => response.json())
+      .then(NewRamen => {
+        console.log("Ramen added:", newRamen);
+        location.reload(); // Refresh the page to reflect the changes (optional)
+      })
+      .catch(error => console.error("Error updating transaction:", error));
+  };
+
+
+  }
+  /*"id": 1,
+        "name": "Shoyu Ramen",
+        "restaurant": "Nonono",
+        "image": "./assets/ramen/shoyu.jpg",
+        "rating": 7,
+        "comment": "Delish. Can't go wrong with a classic!"*/
 }
 
 const displayRamens = () => {
